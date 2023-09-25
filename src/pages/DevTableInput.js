fd1964c1e6c17bd2b289b4f75d7ec5a30c341f11
ega_p.p <ega_p.p@bi.go.id>
2023-09-25 19:26:14 +0700

    Add api call & Render Column From BE

--- src/pages/DevTableInput.js
+++ src/pages/DevTableInput.js
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
+import Box from "@mui/material/Box";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,6 +11,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Checkbox, Select, InputLabel, MenuItem, TextField, FormControl, Button } from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import { getDb, getColumn } from "../services/api";
+import SelectComponent from '../components/SelectComponent';
+import metadataJson from '../metadata.json';
+
 
 const columns = [
     { id: 'name', label: 'Name', minWidth: 170 },
@@ -41,6 +46,13 @@ function arrJP(id, label, ket, isCheck, txt) {
     return { id, label, ket, isCheck, txt };
 }
 
+function columnArr(columnType, columnName, metadata) {
+    let colName = columnName.toUppercase();
+    return { columnType, colName, metadata };
+}
+
+const availMeta = metadataJson.columns;
+
 const metaJenisPenggunaan = [
     arrJP(1, 'Modal Kerja', 'kredit/pembiayaan yang diperuntukkan sebagai modal kerja debitur yang bersangkutan.', false, ''),
     arrJP(2, 'Investasi', 'kredit/pembiayaan yang diperuntukkan sebagai pembelian barang modal dan jasa yang diperlukan guna rehabilitasi, modernisasi, ekspansi, relokasi usaha ', false, ''),
@@ -66,6 +78,15 @@ export default function DevTableInput(props) {
     const [selectedRows, setSelectedRows] = React.useState([]);
     const [strSandi, setStrSandi] = React.useState("");
     const [drows, setDrows] = React.useState([]);
+    const [selectedTable, setSelectedTable] = React.useState(null);
+
+    const [dbTableName, setDBTableName] = React.useState({
+        dbName: "",
+        availableTables: [],
+    });
+    const [tableName, setTableName] = React.useState("");
+    const [availableDBAndTables, setAvailableDBAndTables] = React.useState([]);
+    const [columnData, setColumnData] = React.useState([]);
 
     const [checkboxIds, setCheckboxIds] = React.useState([]);
     const [txtfieldIds, setTxtfieldIds] = React.useState([]);
@@ -154,13 +175,77 @@ export default function DevTableInput(props) {
         }
     }
 
+    function addColumnMeta() {
+        let columnObj = [];
+        let columnData = null;
+        if (columnData.length > 0) {
+            for (var i = 0; i < columnData.length; i++) {
+                //availMeta.map((row) => {
+                if (availMeta.includes(columnData[i].columnName)) {
+                    let index = availMeta.findIndex(el => el === columnData[i].columnName);
+                    columnData = columnArr(columnData[i].columnType, columnData[i].columnName, availMeta[index].metadata)
+                } else {
+                    columnData = columnArr(columnData[i].columnType, columnData[i].columnName, "")
+                }
+                columnObj.push(columnData);
+                //})
+            }
+            setDrows(columnObj);
+        }
+    }
+
+    const handleChangeDbName = (value) => {
+        if (tableName) {
+            
+        }
+        setDBTableName({
+            dbName: value,
+            availableTables: availableDBAndTables.reduce((result, o) => {
+                if (o.dbName === value) result = o.tables;
+                return result;
+            }, []),
+        });
+    };
+
+    const handleChangeTableName = (value) => {
+        let res;
+        if (value) {
+            setTableName(value)
+            getColumn({ db: dbTableName.dbName, table: value })
+                .then((obj) => {
+                    res = obj.columns;
+                    setColumnData(res);
+                    addColumnMeta();
+                    console.log("Columns ", JSON.stringify(res))
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+
     /*handleSandi(event){
         this.setSandiTxtProp(sandiTxtProp);
         event.preventDefault();
     };*/
 
+    const options = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': `Bearer `
+        }
+    }
+
     React.useEffect(() => {
-        mapToDrows();
+        getDb(options)
+            .then(({ db }) => {
+                setAvailableDBAndTables(db);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        //mapToDrows();
         //console.log('Drows : ' + drows);
         //setTimeout(() => mapToDrows, 2000);
         //console.log("Array:" + selectedRows);
@@ -171,6 +256,27 @@ export default function DevTableInput(props) {
     return (
         
         <Paper sx={{ width: '125%', overflow: 'hidden' }}>
+            <Box
+                component="form"
+                sx={{ "& > :not(style)": { m: 1 }, mx: 'auto' }}
+                noValidate
+                autoComplete="off"
+            >
+                <SelectComponent
+                    labelString="Database Name"
+                    objectVariable={dbTableName.dbName}
+                    setVariable={handleChangeDbName}
+                    dataOptions={availableDBAndTables.map((o) => o.dbName)}
+                >
+                </SelectComponent>
+                <SelectComponent
+                    labelString="Table Name"
+                    objectVariable={tableName}
+                    setVariable={handleChangeTableName}
+                    dataOptions={dbTableName.availableTables}
+                    disabled={!dbTableName.dbName}
+                >
+                </SelectComponent>
             <TableContainer sx={{ maxHeight: 440 }}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
@@ -186,13 +292,14 @@ export default function DevTableInput(props) {
                                 />
 
                             </TableCell>
-                            <TableCell align="left" width='15%'>&nbsp; SANDI &nbsp;</TableCell>
-                            <TableCell align="left" width='15%'>&nbsp; LABEL &nbsp;</TableCell>
-                            {/*<TableCell align="left" width='50%'>&nbsp; KETERANGAN &nbsp;</TableCell>*/}
+                                <TableCell align="left" width='15%'>&nbsp; NAMA KOLOM &nbsp;</TableCell>
+                                <TableCell align="left" width='5%'>&nbsp; FILTER &nbsp;</TableCell>
+                                <TableCell align="left" width='5%'>&nbsp; OPERATOR &nbsp;</TableCell>
+                                <TableCell align="left" width='65%'>&nbsp; KONDISI &nbsp;</TableCell>
                         </TableRow>
                     </TableHead>
-                    <TableBody>
-                        {(drows.length > 0)
+                        <TableBody>
+                            {(columnData !== null)
                             ?
                             drows.map((row, index) => (
                                     <TableRow
@@ -287,7 +394,7 @@ export default function DevTableInput(props) {
                 onClick={() => changeSandi(strSandi)}
             >OK</Button>
             <br></br>
-
+            </Box>
         </Paper>
     );
 }
